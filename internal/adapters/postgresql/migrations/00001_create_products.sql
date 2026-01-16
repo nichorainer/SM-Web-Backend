46538f5e0909af5bb15aea72bc5c8ec4a6e53b85
nichorainer <nicholasrainer12@gmail.com>
2026-01-16 18:45:28 +0700

    Add products table migration and update queries.sql for sqlc

--- internal/adapters/postgresql/migrations/00001_create_products.sql
+++ internal/adapters/postgresql/migrations/00001_create_products.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS products (
   id BIGSERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  price_in_centers INTEGER NOT NULL CHECK (price_in_centers >= 0),
+  price_in_idr INTEGER NOT NULL CHECK (price_in_idr >= 0),
   quantity INTEGER NOT NULL DEFAULT 0,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
